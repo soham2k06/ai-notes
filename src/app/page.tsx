62c79ea5602f212824923c1e2d4e7b94d6773574
soham2k06 <sohmmdev@gmail.com>
2024-01-16 11:36:53 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,16 @@ export default function Home() {
   const { userId } = auth();
 
   if (userId) redirect("/notes");
-
+  // test
   return (
-    <main className="flex flex-col h-screen justify-center items-center gap-5">
+    <main className="flex h-screen flex-col items-center justify-center gap-5">
       <div className="flex items-center gap-4">
         <Image src={logo} alt="NoteSwift logo" width={100} height={100} />
-        <span className="font-extrabold tracking-tight text-4xl lg:text-5xl">
+        <span className="text-4xl font-extrabold tracking-tight lg:text-5xl">
           NoteSwift
         </span>
       </div>
-      <p className="text-center max-w-prose">
+      <p className="max-w-prose text-center">
         An intelligent note taking app with AI integration, built with OpenAI,
         Pinecone, Next.js, Shadcn UI, Clerk, and more.
       </p>
