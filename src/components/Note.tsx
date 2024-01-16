57620133e6f2c5e652684cae50b95c536aa4f2a7
soham2k06 <sohmmdev@gmail.com>
2024-01-16 14:07:35 +0530

    implemented edit-delete functionality

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Note as NoteModel } from "@prisma/client";
 import {
   Card,
@@ -6,27 +8,40 @@ import {
   CardHeader,
   CardTitle,
 } from "./ui/card";
+import { useState } from "react";
+import AddEditNoteDialog from "./AddEditNoteDialog";
 interface NoteProps {
   note: NoteModel;
 }
 function Note({ note }: NoteProps) {
+  const [showEditDialog, setShowEditDialog] = useState(false);
   const wasUpdated = note.updatedAt > note.createdAt;
 
   const timestamp = (
     wasUpdated ? note.updatedAt : note.createdAt
   ).toDateString();
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{note.title}</CardTitle>
-        <CardDescription>
-          {timestamp} {wasUpdated && " (updated)"}
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p className="whitespace-pre-line">{note.content}</p>
-      </CardContent>
-    </Card>
+    <>
+      <Card
+        className="cursor-pointer transition-shadow hover:shadow-lg"
+        onClick={() => setShowEditDialog(true)}
+      >
+        <CardHeader>
+          <CardTitle>{note.title}</CardTitle>
+          <CardDescription>
+            {timestamp} {wasUpdated && " (updated)"}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p className="whitespace-pre-line">{note.content}</p>
+        </CardContent>
+      </Card>
+      <AddEditNoteDialog
+        open={showEditDialog}
+        setOpen={setShowEditDialog}
+        noteToEdit={note}
+      />
+    </>
   );
 }
 
