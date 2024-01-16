8ac9d9a4c73bb779a9b83231f66de86ed60f2cbd
soham2k06 <sohmmdev@gmail.com>
2024-01-16 14:08:01 +0530

    implemented dark mode functionality

--- src/app/notes/Nav.tsx
+++ src/app/notes/Nav.tsx
@@ -2,21 +2,28 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/assets/logo.png";
+import logo from "@/assets/logo3.png";
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import { useState } from "react";
-import AddNoteDialog from "@/components/AddNoteDialog";
+import AddNoteDialog from "@/components/AddEditNoteDialog";
+import ThemeToggle from "@/components/ThemeToggle";
 
 function Nav() {
-  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
+  const [showAddEditNoteDialog, setShowAddEditNoteDialog] = useState(false);
   return (
     <>
       <div className="p-4 shadow">
         <div className="mx-auto flex max-w-7xl flex-wrap justify-between gap-3">
           <Link href="/notes" className="flex items-center gap-1">
-            <Image src={logo} alt="NoteSwift logo" width={40} height={40} />
+            <Image
+              src={logo}
+              alt="NoteSwift logo"
+              width={60}
+              height={60}
+              className="dark:invert"
+            />
             <span className="font-bold">NoteSwift</span>
           </Link>
           <div className="flex items-center gap-2">
@@ -26,14 +33,18 @@ function Nav() {
                 elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
               }}
             />
-            <Button onClick={() => setShowAddNoteDialog(true)}>
+            <ThemeToggle />
+            <Button onClick={() => setShowAddEditNoteDialog(true)}>
               <Plus size={20} className="mr-2" />
               Add Note
             </Button>
           </div>
         </div>
       </div>
-      <AddNoteDialog open={showAddNoteDialog} setOpen={setShowAddNoteDialog} />
+      <AddNoteDialog
+        open={showAddEditNoteDialog}
+        setOpen={setShowAddEditNoteDialog}
+      />
     </>
   );
 }
---
+++ src/components/AddEditNoteDialog.tsx
@@ -0,0 +1,157 @@
+import { CreateNoteSchema, createNoteSchema } from "@/lib/validation/note";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import LoadingButton from "./ui/loading-button";
+import { useRouter } from "next/navigation";
+import { Note } from "@prisma/client";
+import { useState } from "react";
+
+interface AddEditNoteDialogProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  noteToEdit?: Note;
+}
+
+function AddEditNoteDialog({
+  open,
+  setOpen,
+  noteToEdit,
+}: AddEditNoteDialogProps) {
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const router = useRouter();
+
+  const form = useForm<CreateNoteSchema>({
+    resolver: zodResolver(createNoteSchema),
+    defaultValues: {
+      title: noteToEdit?.title || "",
+      content: noteToEdit?.content || "",
+    },
+  });
+  const { handleSubmit, control, formState, reset } = form;
+
+  async function onSubmit(data: CreateNoteSchema) {
+    try {
+      if (!!noteToEdit) {
+        const res = await fetch("/api/notes", {
+          method: "PUT",
+          body: JSON.stringify({
+            id: noteToEdit.id,
+            ...data,
+          }),
+        });
+      } else {
+        const res = await fetch("/api/notes", {
+          method: "POST",
+          body: JSON.stringify(data),
+        });
+        if (!res.ok) throw new Error("Status code: " + res.status);
+        reset();
+      }
+
+      router.refresh();
+      setOpen(false);
+    } catch (error) {
+      console.error(error);
+      alert("Something went wrong. Please try again.");
+    }
+  }
+
+  async function deleteNote() {
+    if (!noteToEdit) return;
+    setIsDeleting(true);
+    try {
+      const res = await fetch("/api/notes", {
+        method: "DELETE",
+        body: JSON.stringify({ id: noteToEdit.id }),
+      });
+      if (!res.ok) throw new Error("Status code: " + res.status);
+      router.refresh();
+      setOpen(false);
+    } catch (error) {
+      console.error(error);
+      alert("Something went wrong. Please try again.");
+    } finally {
+      setIsDeleting(false);
+    }
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{noteToEdit ? "Edit" : "Add"} Note</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
+            <FormField
+              control={control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Note Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Note title" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="content"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Note Content</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Note content" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <LoadingButton
+                type="submit"
+                disabled={formState.isSubmitting || isDeleting}
+                loading={formState.isSubmitting}
+              >
+                Submit
+              </LoadingButton>
+              {noteToEdit && (
+                <LoadingButton
+                  variant="destructive"
+                  disabled={formState.isSubmitting || isDeleting}
+                  loading={isDeleting}
+                  onClick={deleteNote}
+                  type="button"
+                >
+                  Delete note
+                </LoadingButton>
+              )}
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default AddEditNoteDialog;
