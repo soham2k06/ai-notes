fcc18ae2fbdcff5d70b70effe77b68df99fe3762
soham2k06 <sohmm.dev@gmail.com>
2024-01-17 20:56:53 +0530

    updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "./ThemeProvider";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <body className={inter.className}>
           <ThemeProvider attribute="class">{children}</ThemeProvider>
         </body>
